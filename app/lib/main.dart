2fbc255576ab7d3947b38dd60216f0144e3fe034
unknown <Administrador@BOMBEIRO.LOCAL>
2019-11-11 14:18:07 -0300

    Alteracao

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,29 +1,14 @@
 import 'package:app/Widgets/AnimationController.dart';
-import 'package:app/tela_principal.dart';
 import 'package:flutter/material.dart';
-
-import 'package:app/Widgets/FlipWidget.dart';
-
-import 'dart:math' as math;
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Moustaine',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Moustaine App'),
@@ -33,18 +18,7 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
@@ -61,27 +35,6 @@ class _MyHomePageState extends State<MyHomePage> {
     super.dispose();
   }
 
-
-
-
-
-
-
-
-
-  void _realizarLogin(){
-
-
-    /*TelaPrincipal telaPrincipal = new TelaPrincipal(controlUsuario.text, controlSenha.text);
-
-    Navigator.push(context,
-    MaterialPageRoute(builder: (context) => telaPrincipal));*/
-
-    setState(() {
-      
-    });
-
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -100,8 +53,6 @@ class _MyHomePageState extends State<MyHomePage> {
                 labelText: 'Usuario'
               ),
             ),
-
-
             TextFormField(
               controller: controlSenha,
               obscureText: true,
@@ -109,20 +60,10 @@ class _MyHomePageState extends State<MyHomePage> {
                 labelText: 'Senha',                
               ),
             ),
-
-            RaisedButton(
-              child: Text("Login"),
-              onPressed: _realizarLogin,
-            ),
-            Page()
-            
-
-
-
+            Page() 
           ],
         ),
-      ),
-     
+      ),     
     );
   }
 }
